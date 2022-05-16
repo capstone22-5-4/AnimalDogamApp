b63f82a019ebd5efacfa3c6eb622d062a4a9c263
jhyoon9705 <jhyoon513@naver.com>
2022-05-16 22:19:25 +0900

    Replace settings screen with habitat screen

---
+++ src/pages/Habitat.tsx
@@ -0,0 +1,14 @@
+import React, { useCallback } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+function Habitat() {
+  return (
+    <View>
+      <Text>서식지도</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default Habitat;
