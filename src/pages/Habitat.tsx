75eed89fa93b16c1000988bcc000c0069a1bbce5
jhyoon9705 <jhyoon513@naver.com>
2022-05-17 14:28:41 +0900

    Load the gps data of photos from the server

--- src/pages/Habitat.tsx
+++ src/pages/Habitat.tsx
@@ -1,7 +1,34 @@
-import React, { useCallback } from 'react';
+import { useIsFocused } from '@react-navigation/native';
+import axios, { AxiosError } from 'axios';
+import React, { useCallback, useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Config from 'react-native-config';
 
 function Habitat() {
+  const [locationData, setLocationData] = useState([]);
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    async function loadLocation() {
+      try {
+        const response = await axios.get(`${Config.API_URL}/gpsData`, {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          withCredentials: true,
+        });
+        setLocationData(response.data);
+        console.log(response.data);
+      } catch (error) {
+        const errorResponse = (error as AxiosError).response;
+        console.log(errorResponse);
+      }
+    }
+
+    loadLocation();
+    console.log(locationData);
+  }, [isFocused]);
+
   return (
     <View>
       <Text>서식지도</Text>
