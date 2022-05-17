42835791d7152104d15a9e856bdeed6dcd51de42
jhyoon9705 <jhyoon513@naver.com>
2022-05-17 23:37:19 +0900

    Load user's location and set the center of the habitat map

--- src/pages/Habitat.tsx
+++ src/pages/Habitat.tsx
@@ -1,17 +1,63 @@
 import { useIsFocused } from '@react-navigation/native';
 import axios, { AxiosError } from 'axios';
-import React, { useCallback, useEffect, useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { PermissionsAndroid, Platform, StyleSheet, View } from 'react-native';
 import Config from 'react-native-config';
 import NaverMapView, { Marker } from 'react-native-nmap';
+import Geolocation from 'react-native-geolocation-service';
+
+async function requestPermission() {
+  try {
+    if (Platform.OS === 'ios') {
+      return await Geolocation.requestAuthorization('always');
+    }
+    if (Platform.OS === 'android') {
+      return await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      );
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
 
 function Habitat() {
+  const [myLocation, setMyLocation] = useState({
+    latitude: 37.564362,
+    longitude: 126.977011,
+  });
   const [locationData, setLocationData] = useState([]);
   const isFocused = useIsFocused();
 
-  const P0 = { latitude: 37.564362, longitude: 126.977011 };
-
   let markerKey = 0;
+
+  useEffect(() => {
+    async function loadMyLocation() {
+      await requestPermission().then((result) => {
+        console.log({ result });
+        if (result === 'granted') {
+          Geolocation.getCurrentPosition(
+            (pos) => {
+              setMyLocation({
+                latitude: pos.coords.latitude,
+                longitude: pos.coords.longitude,
+              });
+            },
+            (error) => {
+              console.log(error);
+            },
+            {
+              enableHighAccuracy: true,
+              timeout: 3600,
+              maximumAge: 3600,
+            },
+          );
+        }
+      });
+    }
+    loadMyLocation();
+    console.log(myLocation);
+  }, []);
 
   useEffect(() => {
     async function loadLocation() {
@@ -44,7 +90,6 @@ function Habitat() {
         image={require('../../images/map_marker.png')}
         width={20}
         height={20}
-        onClick={() => console.log(location.animal_name)}
         caption={{
           text: location.animal_name,
           minZoom: 16,
@@ -55,12 +100,13 @@ function Habitat() {
     ),
   );
 
+  console.log(myLocation);
   return (
     <View>
       <NaverMapView
         style={styles.mapView}
         showsMyLocationButton={true}
-        center={{ ...P0, zoom: 16 }}
+        center={{ ...myLocation }}
       >
         {markerList}
       </NaverMapView>
