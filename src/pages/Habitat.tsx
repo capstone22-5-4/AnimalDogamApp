aac96fa2c6f106a876a9e6734042168bb59ef3ff
jhyoon9705 <jhyoon513@naver.com>
2022-05-19 17:25:37 +0900

    Replace user's gps data with sample data to test in emulator

--- src/pages/Habitat.tsx
+++ src/pages/Habitat.tsx
@@ -39,8 +39,11 @@ function Habitat() {
           Geolocation.getCurrentPosition(
             (pos) => {
               setMyLocation({
-                latitude: pos.coords.latitude,
-                longitude: pos.coords.longitude,
+                // latitude: pos.coords.latitude,
+                // longitude: pos.coords.longitude,
+                // 임시로 CAU 위치로 설정, 나중에 아래 좌표 지우고 위의 사용자 gps 데이터 사용
+                latitude: 37.5051,
+                longitude: 126.9571,
               });
             },
             (error) => {
@@ -105,7 +108,7 @@ function Habitat() {
     <View>
       <NaverMapView
         style={styles.mapView}
-        showsMyLocationButton={true}
+        showsMyLocationButton={false}
         center={{ ...myLocation }}
       >
         {markerList}
